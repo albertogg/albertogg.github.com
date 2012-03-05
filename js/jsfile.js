19d7450edc8439a4ab7a5bde8e412d6888c1e13b
albertogg <albertogrespan@gmail.com>
2012-03-05 18:51:15 -0430

    popover icons, js files

---
+++ js/jsfile.js
@@ -0,0 +1,17 @@
+$(function () { 
+  $("#github")
+    .popover({
+      placement: 'bottom',
+      content: 'Github'
+    });
+  $("#twittel")
+    .popover({
+      placement: 'bottom',
+      content: 'Twitter'
+    });
+  $("#google")
+    .popover({
+      placement: 'bottom',
+      content: 'Google+'
+    });
+});
