5f37f490f7652d51f3ea1436ac68cce4546ad608
albertogg <albertogrespan@gmail.com>
2013-06-23 17:18:01 -0430

    New page header.
    Add new page header.
    
    *remove twitter bootstrap
    *add neat.

--- js/jsfile.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("#github").popover({placement:"bottom",content:"Github"});$("#twittel").popover({placement:"bottom",content:"Twitter"});$("#google").popover({placement:"bottom",content:"Google+"})});
