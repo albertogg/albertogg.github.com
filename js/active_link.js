24909e799506eb8960b822ec80885509106684af
albertogg <albertogrespan@gmail.com>
2014-03-08 15:54:14 -0430

    Validate variable not undefined
    Validating that selectorClass is not undefined to prevent error while searching for object parentNode.

--- js/active_link.js
+++ js/active_link.js
@@ -1,12 +1,14 @@
 document.onreadystatechange = function () {
   if (document.readyState == "complete") {
     var customSelector = document.querySelectorAll('a[href="' + this.location.pathname +'"]');
-      var selectorClass = customSelector[0].parentNode;
-      if (selectorClass.parentNode.classList == 'menu') {
-        if (selectorClass.classList)
-          selectorClass.classList.add('active');
-        else
-          selectorClass.className += ' ' + 'active';
+      if(typeof customSelector[0] !== "undefined") {
+        var selectorClass = customSelector[0].parentNode;
+        if (selectorClass.parentNode.classList == 'menu') {
+          if (selectorClass.classList)
+            selectorClass.classList.add('active');
+          else
+            selectorClass.className += ' ' + 'active';
+        }
       }
    }
 }
